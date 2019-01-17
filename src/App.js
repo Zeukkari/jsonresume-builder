6ffbe0d4f917b53e72f5044bea02a1df3ab6fc3e
Timo Aho <zeukkari@gmail.com>
2019-01-18 01:40:24 +0200

    linting

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const defaultValue = {
   value: '{}',
   message: '',
   setData: () => (null),
-}
+};
 
 const AppContext = React.createContext(defaultValue);
 
@@ -42,45 +42,45 @@ const AppBar = props => (
 const SchemaReference = props => <TextArea value={props.value} {...props} fill readOnly />;
 
 const Basics = ({ basics }) => {
-
-  const name = basics.name || 'N/A'
-  const label = basics.label || 'N/A'
-  const summary = basics.summary || 'N/A'
-  const email = basics.email || 'N/A'
-  const website = basics.website || 'N/A'
-  const picture = basics.picture || 'N/A'
-  const profiles = basics.profiles || []
+  const name = basics.name || 'N/A';
+  const label = basics.label || 'N/A';
+  const summary = basics.summary || 'N/A';
+  const email = basics.email || 'N/A';
+  const website = basics.website || 'N/A';
+  // const picture = basics.picture || 'N/A';
+  // const profiles = basics.profiles || [];
 
 
   return (
-  <div>
-    <p>name: {name}</p>
-    <p>label: {label}</p>
-    <p>summary: {summary}</p>
-    <p>email: {email}</p>
-    <p>website: {website}</p>
-  </div>
-  )
-}
+    <div>
+      <p>name: {name}</p>
+      <p>label: {label}</p>
+      <p>summary: {summary}</p>
+      <p>email: {email} </p>
+      <p>website: {website}</p>
+    </div>
+  );
+};
 
-const WorkEntry = ({ company, position, startDate, endDate, summary, highlights }) => (
+const WorkEntry = ({
+  company, position, startDate, endDate, summary, highlights,
+}) => (
   <div>
-    <p>company: {company}</p>
+    <p>company: {company} </p>
     <p>position: {position}</p>
     <p>startDate: {startDate}</p>
-    <p>endDate: {endDate}</p>
+    <p>endDate: {endDate}
+    </p>
     <p>summary: {summary}</p>
     <ul>
-      {highlights.map(item =>
-        <li key={item}>{item}</li>
-      )}
+      {highlights.map(item => <li key={item}>{item}</li>)}
     </ul>
   </div>
-)
+);
 
 const Work = ({ worklist }) => (
   <div>
-    {worklist.map(item =>
+    {worklist.map(item => (
       <WorkEntry
         key={`${item.startDate}-${item.endDate}`}
         company={item.company}
@@ -90,55 +90,58 @@ const Work = ({ worklist }) => (
         summary={item.summary}
         highlights={item.highlights}
       />
-    )}
+    ))}
   </div>
-)
+);
 
 const SkillsEntry = ({ name, level, keywords }) => (
   <div key={name}>
-    <p>name: {name}</p>
-    <p>level: {level}</p>
+    <p>
+name:
+      {name}
+    </p>
+    <p>
+level:
+      {level}
+    </p>
     <ul>
-      {keywords.map(item =>
-        <li key={item}>{item}</li>
-      )}
+      {keywords.map(item => <li key={item}>{item}</li>)}
     </ul>
   </div>
-)
+);
 
-const Skills = ({ skills }) => {
-  return (
-    <div>
-      {skills.map(item =>
-        <SkillsEntry
-          key={item.name}
-          level={item.level}
-          keywords={item.keywords}
-        />
-      )}
-    </div>
-  )
-}
+const Skills = ({ skills }) => (
+  <div>
+    {skills.map(item => (
+      <SkillsEntry
+        key={item.name}
+        level={item.level}
+        keywords={item.keywords}
+      />
+    ))}
+  </div>
+);
 
-const PreviewResume = props => {
-  if(!props.data.isValid) {
-    return(
-    <Box
-      flex
-      full
-      justify="center"
-      wrap={false}
-      height="medium"
-      overflow="scroll"
-      border={{ color: 'brand', size: 'medium' }}>
-      <Heading>Error rendering</Heading>
-    </Box>
-    )
+const PreviewResume = (props) => {
+  if (!props.data.isValid) {
+    return (
+      <Box
+        flex
+        full
+        justify="center"
+        wrap={false}
+        height="medium"
+        overflow="scroll"
+        border={{ color: 'brand', size: 'medium' }}
+      >
+        <Heading>Error rendering</Heading>
+      </Box>
+    );
   }
 
-  const actualData = JSON.parse(props.data.value)
+  const actualData = JSON.parse(props.data.value);
 
-  console.log("actualData: ", actualData)
+  console.log('actualData: ', actualData);
 
   const basics = actualData.basics;
 
@@ -163,68 +166,68 @@ const PreviewResume = props => {
   const projects = actualData.projects;
 
   return (
-  <Box flex overflow="scroll" border={{ color: 'brand', size: 'medium' }}>
-    <Heading size="xlarge">Preview Resume</Heading>
+    <Box flex overflow="scroll" border={{ color: 'brand', size: 'medium' }}>
+      <Heading size="xlarge">Preview Resume</Heading>
 
-    <Heading> Basics </Heading>
+      <Heading> Basics </Heading>
 
-    {basics &&
-      <Basics basics={basics} />
+      {basics
+      && <Basics basics={basics} />
     }
 
-    <Heading> Work </Heading>
+      <Heading> Work </Heading>
 
-    {work && (
+      {work && (
       <Work worklist={work} />
       )
     }
 
-    <Heading> Skills </Heading>
-    {skills &&
-      <Skills skills={skills} />
+      <Heading> Skills </Heading>
+      {skills
+      && <Skills skills={skills} />
     }
 
-    <Heading> Interests </Heading>
-    {interests &&
-      <div>placeholder</div>
+      <Heading> Interests </Heading>
+      {interests
+      && <div>placeholder</div>
     }
-    <Heading> Education </Heading>
-    {education &&
-      <div>education</div>
-    }
-
-    <Heading> Languages </Heading>
-    {languages &&
-      <div>languages</div>
+      <Heading> Education </Heading>
+      {education
+      && <div>education</div>
     }
 
-    <Heading> Awards </Heading>
-    {awards &&
-      <div>awards</div>
+      <Heading> Languages </Heading>
+      {languages
+      && <div>languages</div>
     }
 
-    <Heading> Projects </Heading>
-    {projects &&
-      <div>projects</div>
+      <Heading> Awards </Heading>
+      {awards
+      && <div>awards</div>
     }
 
-    <Heading> Publications </Heading>
-    {publications &&
-      <div>publications</div>
+      <Heading> Projects </Heading>
+      {projects
+      && <div>projects</div>
     }
 
-    <Heading> References </Heading>
-    {references &&
-      <div>references</div>
+      <Heading> Publications </Heading>
+      {publications
+      && <div>publications</div>
     }
 
-    <Heading> Volunteer </Heading>
-    {volunteer &&
-      <div>volunteer</div>
+      <Heading> References </Heading>
+      {references
+      && <div>references</div>
     }
-  </Box>
+
+      <Heading> Volunteer </Heading>
+      {volunteer
+      && <div>volunteer</div>
+    }
+    </Box>
   );
-}
+};
 
 /*
 const ResumeJsonInput = () => (
@@ -249,44 +252,44 @@ class ResumeJsonInput extends Component {
     message: '',
   };
 
-  onChange = event => {
-    let context = this.context;
-    console.log("context: ", context)
+  onChange = (event) => {
+    const context = this.context;
+    console.log('context: ', context);
     const inputValue = event.target.value;
     let data;
     try {
       data = JSON.parse(event.target.value);
-    } catch(e) {
+    } catch (e) {
       this.props.setData({
         isValid: false,
-        message: "invalid json",
-        value: inputValue
+        message: 'invalid json',
+        value: inputValue,
       });
       return;
     }
 
     const cb = (err, valid) => {
-      if(err != undefined || valid == undefined) {
+      if (err != undefined || valid == undefined) {
         this.props.setData({ isValid: false, message: err[0].message, value: inputValue });
         return;
-      };
-      this.props.setData({isValid: true, value: inputValue});
-      return;
+      }
+      this.props.setData({ isValid: true, value: inputValue });
     };
     this.props.validate(data, cb);
-
   }
 
   render() {
+    console.log('render....', this.props);
+    const data = this.props.data || { isValid: false, message: 'foobar', value: 'foo' };
 
-    console.log("render....", this.props)
-    const data = this.props.data || { isValid: false, message: "foobar", value: "foo" };
-
-    console.log("data: ", data)
+    console.log('data: ', data);
     const label = data && data.isValid ? (
-      <Text style={{color: 'green'}}>Input seems legit</Text>
+      <Text style={{ color: 'green' }}>Input seems legit</Text>
     ) : (
-      <Text style={{color: 'red'}}>Error: {data && data.message}</Text>
+      <Text style={{ color: 'red' }}>
+Error:
+        {data && data.message}
+      </Text>
     );
 
     return (
@@ -298,10 +301,7 @@ class ResumeJsonInput extends Component {
   }
 }
 
-const InsertJson = props => <TextArea value={props.value} {...props} fill readOnly />;
-
 class App extends Component {
-
   constructor(props, context) {
     super(props, context);
 
@@ -313,20 +313,19 @@ class App extends Component {
         isValid: true,
         value: '{}',
         message: '',
-      }
-    }
+      },
+    };
 
     this.togglePreview = this.togglePreview.bind(this);
     this.toggleSidebar = this.toggleSidebar.bind(this);
 
     this.setData = (data) => {
-      console.log("setting state", data)
+      console.log('setting state', data);
       this.setState({
         ...this.state,
-        data: data
-      })
-    }
-
+        data,
+      });
+    };
   }
 
   /*
@@ -349,35 +348,39 @@ class App extends Component {
   */
 
   toggleSidebar() {
-    console.log("toggle sidebar")
+    console.log('toggle sidebar');
     const oldState = this.state;
-    if(oldState.showSidebar) {
-      this.setState({ ...oldState, showSidebar: false })
+    if (oldState.showSidebar) {
+      this.setState({ ...oldState, showSidebar: false });
       return;
     }
-    this.setState({ ...oldState, showPreview: false, showSidebar: true, showEditor: true })
+    this.setState({
+      ...oldState, showPreview: false, showSidebar: true, showEditor: true,
+    });
   }
 
   togglePreview() {
-    console.log("toggle preview")
+    console.log('toggle preview');
     const oldState = this.state;
-    if(oldState.showPreview) {
-      this.setState({ ...oldState, showPreview: false, showEditor: true })
+    if (oldState.showPreview) {
+      this.setState({ ...oldState, showPreview: false, showEditor: true });
       return;
     }
-    this.setState({ ...oldState, showPreview: true, showSidebar: false, showEditor: false })
+    this.setState({
+      ...oldState, showPreview: true, showSidebar: false, showEditor: false,
+    });
   }
 
   render() {
     const { validate, schema } = this.props;
     const schemaString = JSON.stringify(schema, null, 4);
-    const { showSidebar, showPreview, showEditor } = this.state;
+    const { showSidebar, showPreview } = this.state;
 
     const { setData } = this;
     const data = this.state.data;
 
 
-/*
+    /*
       <AppState.Provider
         value={{
           setData: this.setData,
@@ -394,15 +397,15 @@ class App extends Component {
       <AppContext.Provider
         value={{ data, setData }}
       >
-      <Grommet theme={theme} full>
-        <Box fill>
-          <AppBar>
+        <Grommet theme={theme} full>
+          <Box fill>
+            <AppBar>
               <Heading level="3" margin="none">
                 Resume Builder
               </Heading>
               <div>
                 <Button
-                  icon={this.state.showSidebar ? (<Contract color="white" />) : (<Expand color="white" />) }
+                  icon={this.state.showSidebar ? (<Contract color="white" />) : (<Expand color="white" />)}
                   onClick={this.toggleSidebar}
                 />
                 <Button
@@ -413,45 +416,46 @@ class App extends Component {
             </AppBar>
             <Box
               flex
-              align='center'
-              justify='between'
-              direction='row'
+              align="center"
+              justify="between"
+              direction="row"
             >
               <AppContext.Consumer>
                 {({ data, setData }) => (
-                  <ResumeJsonInput  {...this.props} data={data} setData={setData} />
+                  <ResumeJsonInput {...this.props} data={data} setData={setData} validate={validate} />
                 )}
 
               </AppContext.Consumer>
-              <Collapsible direction="horizontal"  open={showSidebar}>
+              <Collapsible direction="horizontal" open={showSidebar}>
                 <Box
                   flex
-                  width='large'
-                  background='light-2'
-                  elevation='small'
-                  align='stretch'
-                  justify='between'
+                  width="large"
+                  background="light-2"
+                  elevation="small"
+                  align="stretch"
+                  justify="between"
                 >
                   <SchemaReference value={schemaString} />
                 </Box>
               </Collapsible>
             </Box>
           </Box>
-          <Box direction='row' flex overflow={{ horizontal: 'hidden' }} >
-          <Layer
-            full
-            margin="large"
-            position={showPreview ? 'center' : 'hidden'}
-            onClickOutside={this.togglePreview}>
-            <AppContext.Consumer>
-              {({ data }) => (
-                <PreviewResume  {...this.props} data={data} />
-              )}
-            </AppContext.Consumer>
-          </Layer>
+          <Box direction="row" flex overflow={{ horizontal: 'hidden' }}>
+            <Layer
+              full
+              margin="large"
+              position={showPreview ? 'center' : 'hidden'}
+              onClickOutside={this.togglePreview}
+            >
+              <AppContext.Consumer>
+                {({ data }) => (
+                  <PreviewResume {...this.props} data={data} />
+                )}
+              </AppContext.Consumer>
+            </Layer>
           </Box>
 
-      </Grommet>
+        </Grommet>
       </AppContext.Provider>
     );
   }
