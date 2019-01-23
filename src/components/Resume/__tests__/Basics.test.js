31017ac78c9b95e4b0018228271431874e6f0688
Timo Aho <zeukkari@gmail.com>
2019-01-23 21:04:39 +0200

    update snapshots

--- src/components/Resume/__tests__/Basics.test.js
+++ src/components/Resume/__tests__/Basics.test.js
@@ -1,16 +1,37 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
+import React from 'react'
+import renderer from 'react-test-renderer'
 
-import Basics from '../Basics';
+import { Contact, Summary, Picture } from '../Basics'
 
-import defaultResume from '../../../util/defaultResume';
+import defaultResume from '../../../util/defaultResume'
 
-
-test('renders basics section', () => {
-  const data = defaultResume.basics;
+test('renders basics section summary', () => {
+  const { name, label, summary } = defaultResume.basics
   const component = renderer.create(
-    <Basics basics={data} />
-  );
-  let tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
-});
+    <Summary name={name} label={label} summary={summary} />,
+  )
+  let tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+test('renders basics section picture', () => {
+  const picture = defaultResume.basics.picture
+  const component = renderer.create(<Picture picture={picture} />)
+  let tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+test('renders basics section contact info', () => {
+  const { email, phone, location, website, profiles } = defaultResume.basics;
+  const component = renderer.create(
+    <Contact
+      email={email}
+      phone={phone}
+      location={location}
+      website={website}
+      profiles={profiles}
+    />)
+
+  let tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+})
