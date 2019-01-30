3dca16499b71cc56655c5805fd12ab057d9dc17a
Timo Aho <zeukkari@gmail.com>
2019-01-30 20:25:59 +0200

    new editor

--- packages/jsonresume-builder/src/components/Editor.js
+++ packages/jsonresume-builder/src/components/Editor.js
@@ -1,66 +1,84 @@
 import React, { Component } from 'react'
 
+import { Box } from 'grommet'
+
 import ace from 'brace'
+import AceEditor from 'react-ace'
 import 'brace/mode/json'
 import 'brace/theme/monokai'
+import 'brace/theme/github'
+import 'brace/theme/tomorrow'
+import 'brace/theme/kuroir'
+import 'brace/theme/twilight'
+import 'brace/theme/xcode'
+import 'brace/theme/textmate'
+import 'brace/theme/solarized_dark'
+import 'brace/theme/solarized_light'
+import 'brace/theme/terminal'
 
 import Ajv from 'ajv'
 import draft4 from 'ajv/lib/refs/json-schema-draft-04.json'
 
-import { JsonEditor as Editor } from 'jsoneditor-react'
-import 'jsoneditor-react/es/editor.min.css'
-
-import defaultResume from '../util/defaultResume'
-
-const style = {
-  width: '100%',
-  height: '100%',
-}
-
-export function Decorator({ children }) {
-  return <div style={style}>{children}</div>
-}
-
-const ajv = new Ajv({ schemaId: 'id' })
-ajv.addMetaSchema(draft4)
+import { schema } from 'resume-schema'
 
 class ResumeJsonInput extends Component {
   constructor(props) {
     super(props)
+
+    this.ajv = new Ajv({
+      schemaId: 'id',
+      allErrors: true,
+      verbose: true,
+      format: 'full',
+    })
+    this.ajv.addMetaSchema(draft4)
+
+    this.editorRef = React.createRef()
+
+    this.onChange = this.onChange.bind(this)
   }
 
-  onChange = resumeObject => {
-    this.setState(resumeObject)
-    const cb = (err, valid) => {
-      if (err != undefined || valid == undefined) {
-        const message =
-          err && err.message ? err.message : 'Generic error message'
-        this.props.setData({ isValid: false, value: { error: message } })
-        return
-      }
-      this.props.setData({
-        isValid: true,
-        value: resumeObject,
-      })
-    }
+  onChange = event => {
+    console.log('onChange event')
+    // console.log('newValue: ', event)
 
-    this.props.validate(resumeObject, cb)
-    return
+    const editor = this.editorRef.current.editor
+    console.log('editor : ', editor)
+
+    try {
+      const newValue = JSON.parse(event)
+
+      const valid = this.ajv.validate(schema, newValue)
+      if (!valid) {
+        throw errors
+        const errors = this.ajv.errors
+        throw errors
+      }
+    } catch {
+      throw 'invalid json'
+    }
   }
 
   render() {
+    const { value } = this.props
+    const userInput = JSON.stringify(value, null, 2)
+
     return (
-      <Decorator>
-        <Editor
-          value={defaultResume}
+      <Box flex direction="column">
+        <Box background="dark-1">Statusbar</Box>
+        <AceEditor
+          ref={this.editorRef}
+          mode="json"
+          theme="monokai"
           onChange={this.onChange}
-          mode={Editor.modes.code}
-          ajv={ajv}
-          validate
-          ace={ace}
-          schema={this.props.schema}
+          name="UNIQUE_ID_OF_DIV"
+          value={userInput}
+          fontSize="24"
+          width="100%"
+          showGutter={true}
+          editorProps={{ $blockScrolling: true }}
         />
-      </Decorator>
+      </Box>
     )
   }
 }
